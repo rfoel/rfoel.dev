d021c67adac31a745fbf4e10baff687060dee774
Rafael <rafaelfr@outlook.com>
2020-08-25 23:25:08 -0300

    chore: add splitbee

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-splitbee`,
+      options: {
+        includeInDevelopment: false,
+        delayTimeout: 0,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
